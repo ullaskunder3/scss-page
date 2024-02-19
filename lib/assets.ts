574e04fcd34d0bb409d8518e53adb2bfba6c491a
Ullas Kunder <ullaskunder3@gmail.com>
2024-02-19 10:38:29 +0530

    initial starter... cleanup/folder structure added assets

---
+++ lib/assets.ts
@@ -0,0 +1,5 @@
+import laptopimage1 from "./img/laptop1.webp";
+import laptopimage2 from "./img/laptop2.webp";
+import laptopimage3 from "./img/laptop3.webp";
+
+export {laptopimage1, laptopimage2, laptopimage3}
